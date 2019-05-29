60bd43852fb3f1c0be05128cb1f3fd9f8f541508
Jakob Löhnertz <mail@jakob.codes>
2019-05-29 13:28:21 +0200

    Add a class that stems terms in parsed documents

---
+++ src/main/kotlin/stemming/StemRetriever.kt
@@ -0,0 +1,42 @@
+package codes.jakob.semanticcoupling.stemming
+
+import codes.jakob.semanticcoupling.model.Corpus
+import codes.jakob.semanticcoupling.model.Document
+import codes.jakob.semanticcoupling.model.NaturalLanguage
+import codes.jakob.semanticcoupling.stemming.StemRetriever.Constants.EndingsBasePath
+import codes.jakob.semanticcoupling.stemming.StemRetriever.Constants.NaturalLanguagesStopWordPath
+import codes.jakob.semanticcoupling.utility.Utilities.getResourceAsText
+import codes.jakob.semanticcoupling.utility.Utilities.isNonEmptyWordEntry
+
+
+class StemRetriever(private val naturalLanguage: NaturalLanguage, private val corpus: Corpus) {
+    fun stemDocuments(): Corpus {
+        val concatinatedRegex: Regex = buildConcatinatedRegex()
+
+        val documents: ArrayList<Document> = ArrayList(corpus.documents.map { document ->
+            Document(
+                name = document.name,
+                content = document.content.map { it.replace(concatinatedRegex, "") }.filter { isNonEmptyWordEntry(it) }
+            )
+        })
+
+        return Corpus(documents)
+    }
+
+    private fun buildConcatinatedRegex(): Regex {
+        return retrieveAllEndings()
+            .joinToString("") { "(($it)\$)|" }
+            .removeSuffix("|")
+            .toRegex()
+    }
+
+    private fun retrieveAllEndings(): List<String> {
+        val endingsFile: String = getResourceAsText("$EndingsBasePath/$NaturalLanguagesStopWordPath/${naturalLanguage.name.toLowerCase()}.txt")
+        return endingsFile.split("\n").filter { it != "" }
+    }
+
+    object Constants {
+        const val EndingsBasePath = "endings"
+        const val NaturalLanguagesStopWordPath = "naturallanguages"
+    }
+}
