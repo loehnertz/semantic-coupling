4568ca44f89d8f1a105060ee46d6e1dd20a0d491
Jakob Löhnertz <mail@jakob.codes>
2019-05-29 16:08:50 +0200

    Add a new data class for a term in a document

---
+++ src/main/kotlin/model/Term.kt
@@ -0,0 +1,22 @@
+package codes.jakob.semanticcoupling.model
+
+
+data class Term(
+    val word: String,
+    var tfidf: Double? = null
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Term
+
+        if (word != other.word) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return 31 * word.hashCode()
+    }
+}
