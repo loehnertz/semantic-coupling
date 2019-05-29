6e1edffebc6b6923a18745c96d54af484530a65b
Jakob Löhnertz <mail@jakob.codes>
2019-05-29 16:20:38 +0200

    Add two getter-methods to retrieve the calculated similarities as non-Kotlin types

--- src/main/kotlin/SemanticCouplingCalculator.kt
+++ src/main/kotlin/SemanticCouplingCalculator.kt
@@ -15,7 +15,9 @@ import codes.jakob.semanticcoupling.tfidf.TfIdfCalculator
 class SemanticCouplingCalculator(private val files: List<Map<String, String>>, private val programmingLanguage: ProgrammingLanguage, private val naturalLanguage: NaturalLanguage = DefaultNaturalLanguage) {
     constructor(files: List<Map<String, String>>, selectedProgrammingLanguage: String, selectedNaturalLanguage: String) : this(files, getProgrammingLanguageByName(selectedProgrammingLanguage), getNaturalLanguageByName(selectedNaturalLanguage))
 
-    fun calculate(): List<Triple<String, String, Double>> {
+    private val similarities: ArrayList<Triple<String, String, Double>> = arrayListOf()
+
+    fun calculate() {
         var corpus = Corpus()
         for (file: Map<String, String> in files) {
             for ((fileName: String, fileContents: String) in file) {
@@ -27,9 +29,17 @@ class SemanticCouplingCalculator(private val files: List<Map<String, String>>, p
         corpus = StemRetriever(naturalLanguage, corpus).stemDocuments()
         corpus = TfIdfCalculator(corpus).calculateForAllTerms()
 
-        val similarities: List<Triple<Document, Document, Double>> = SimilarityCalculator(corpus).calculateDocumentSimilarities()
+        val documentSimilarities: List<Triple<Document, Document, Double>> = SimilarityCalculator(corpus).calculateDocumentSimilarities()
 
-        return similarities.map { Triple(it.first.name, it.second.name, it.third) }
+        documentSimilarities.forEach { similarities.add(Triple(it.first.name, it.second.name, it.third)) }
+    }
+
+    fun retrieveSimilaritiesAsListOfTriples(): List<Triple<String, String, Double>> {
+        return similarities.toList()
+    }
+
+    fun retrieveSimilaritiesAsListsOfLists(): List<List<String>> {
+        return similarities.map { listOf(it.first, it.second, it.third.toString()) }
     }
 
     private fun parseFile(fileName: String, fileContents: String): Document {
