4528f7ad3a1fd5df9e60fd4eabbeeb017b328253
Jakob Löhnertz <mail@jakob.codes>
2019-05-29 16:09:11 +0200

    Implement an 'equals' method for the 'Document' data class

--- src/main/kotlin/model/Document.kt
+++ src/main/kotlin/model/Document.kt
@@ -3,5 +3,20 @@ package codes.jakob.semanticcoupling.model
 
 data class Document(
     val name: String,
-    val content: List<String>
-)
+    val terms: List<Term>
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Document
+
+        if (name != other.name) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return 31 * name.hashCode()
+    }
+}
