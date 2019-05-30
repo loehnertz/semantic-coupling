58eaf9de1fbfb8327b4ed1e051be523d89ba3faa
Jakob Löhnertz <mail@jakob.codes>
2019-05-30 13:56:25 +0200

    Rename the 'StemRetriever' and let it implement the new 'Normalizer' interface

--- src/main/kotlin/normalization/Stemmer.kt
+++ src/main/kotlin/normalization/Stemmer.kt
@@ -1,30 +1,16 @@
 package codes.jakob.semanticcoupling.normalization
 
-import codes.jakob.semanticcoupling.model.Corpus
-import codes.jakob.semanticcoupling.model.Document
 import codes.jakob.semanticcoupling.model.NaturalLanguage
-import codes.jakob.semanticcoupling.model.Term
-import codes.jakob.semanticcoupling.stemming.StemRetriever.Constants.EndingsBasePath
-import codes.jakob.semanticcoupling.stemming.StemRetriever.Constants.NaturalLanguagesStopWordPath
+import codes.jakob.semanticcoupling.normalization.Stemmer.Constants.EndingsBasePath
+import codes.jakob.semanticcoupling.normalization.Stemmer.Constants.NaturalLanguagesStopWordPath
 import codes.jakob.semanticcoupling.utility.Utilities.getResourceAsText
-import codes.jakob.semanticcoupling.utility.Utilities.isNonEmptyWordEntry
 
 
-class StemRetriever(private val naturalLanguage: NaturalLanguage, private val corpus: Corpus) {
-    fun stemDocuments(): Corpus {
-        val concatinatedRegex: Regex = buildConcatinatedRegex()
+class Stemmer(private val naturalLanguage: NaturalLanguage) : Normalizer {
+    private val concatinatedRegex: Regex = buildConcatinatedRegex()
 
-        val documents: ArrayList<Document> = ArrayList(corpus.documents.map { document ->
-            Document(
-                name = document.name,
-                terms = document.terms
-                    .map { it.word.replace(concatinatedRegex, "") }
-                    .filter { isNonEmptyWordEntry(it) }
-                    .map { Term(word = it) }
-            )
-        })
-
-        return Corpus(documents)
+    override fun normalizeWord(word: String): String {
+        return word.replace(concatinatedRegex, "")
     }
 
     private fun buildConcatinatedRegex(): Regex {
