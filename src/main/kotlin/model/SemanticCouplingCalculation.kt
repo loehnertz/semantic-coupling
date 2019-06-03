8b3b0c4973bcbe359c47920e6853ca8e4ce44775
Jakob Löhnertz <mail@jakob.codes>
2019-06-03 10:40:47 +0200

    Remove an unused data class

--- src/main/kotlin/model/SemanticCouplingCalculation.kt
+++
@@ -1,8 +0,0 @@
-package codes.jakob.semanticcoupling.model
-
-
-data class SemanticCouplingCalculation(
-    val documentPairs: List<SemanticCoupling>
-) {
-    fun getSemanticCoupling(documentOne: Document, documentTwo: Document): SemanticCoupling? = documentPairs.firstOrNull { it.documents == Pair(documentOne, documentTwo) || it.documents == Pair(documentTwo, documentOne) }
-}
