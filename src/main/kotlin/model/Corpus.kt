0b5021706b6f7e68875a6edb7353f5e16dbf805a
Jakob Löhnertz <mail@jakob.codes>
2019-06-06 21:20:30 +0200

    Change the 'Corpus' class to use a set of 'Document' instead of a list

--- src/main/kotlin/model/Corpus.kt
+++ src/main/kotlin/model/Corpus.kt
@@ -2,5 +2,5 @@ package codes.jakob.semanticcoupling.model
 
 
 data class Corpus(
-    val documents: ArrayList<Document> = arrayListOf()
+    val documents: MutableSet<Document> = mutableSetOf()
 )
