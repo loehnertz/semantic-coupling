d4926d0cd14e92abcc75b3cf3e29d8a36dcbb791
Jakob Löhnertz <mail@jakob.codes>
2019-06-06 21:22:17 +0200

    Adapt the 'SimilarityCalculator' for the new 'LSI' implementation

--- src/main/kotlin/similarity/SimilarityCalculator.kt
+++ src/main/kotlin/similarity/SimilarityCalculator.kt
@@ -1,53 +1,79 @@
 package codes.jakob.semanticcoupling.similarity
 
+import codes.jakob.semanticcoupling.lsi.LatentSemanticIndexer
 import codes.jakob.semanticcoupling.model.Corpus
 import codes.jakob.semanticcoupling.model.Document
 import codes.jakob.semanticcoupling.model.SemanticCoupling
 
 
-class SimilarityCalculator(private val corpus: Corpus, private val documentSimilaritiesToCalculate: List<Pair<String, String>>?) {
+class SimilarityCalculator(private val corpus: Corpus, private val documentSimilaritiesToCalculate: List<Pair<String, String>>?, private val useLsi: Boolean, numberOfLsiDimensions: Int, maxLsiEpochs: Int) {
+    private lateinit var latentSemanticIndexer: LatentSemanticIndexer
+
+    init {
+        if (useLsi) latentSemanticIndexer = LatentSemanticIndexer(corpus = corpus, dimensions = numberOfLsiDimensions, maxEpochs = maxLsiEpochs)
+    }
+
     fun calculateDocumentSimilarities(): List<SemanticCoupling> {
         val documentSimilarities: ArrayList<SemanticCoupling> = arrayListOf()
 
-        for (document1: Document in corpus.documents) {
-            for (document2: Document in corpus.documents) {
-                if (document1 == document2) continue
-                if (!documentSimilarityShouldBeCalculated(document1, document2)) continue
-                if (documentSimilarities.firstOrNull { documentTripleSimilarityAlreadyCalculated(it, document1, document2) } != null) continue
+        for (documentA: Document in corpus.documents) {
+            for (documentB: Document in corpus.documents) {
+                if (documentA == documentB) continue
+                if (!documentSimilarityShouldBeCalculated(documentA, documentB)) continue
+                if (documentSimilarities.firstOrNull { documentTripleSimilarityAlreadyCalculated(it, documentA, documentB) } != null) continue
 
-                val allWords: List<String> = (document1.terms + document2.terms).map { it.word }.distinct().sorted()
+                val documentSimilarity: Double = calculateDocumentSimilarity(documentA, documentB)
+                val semanticCoupling = SemanticCoupling(documents = Pair(documentA, documentB), score = documentSimilarity)
 
-                val document1WordVector: ArrayList<Double> = arrayListOf()
-                val document2WordVector: ArrayList<Double> = arrayListOf()
-
-                for (word: String in allWords) {
-                    document1WordVector.add(document1.terms.find { it.word == word }?.tfidf ?: 0.0)
-                    document2WordVector.add(document2.terms.find { it.word == word }?.tfidf ?: 0.0)
-                }
-
-                val cosineSimilarity: Double = calculateCosineSimilarity(document1WordVector.toTypedArray(), document2WordVector.toTypedArray())
-                documentSimilarities.add(SemanticCoupling(documents = Pair(document1, document2), score = cosineSimilarity))
+                documentSimilarities.add(semanticCoupling)
             }
         }
 
         return documentSimilarities.toList().sortedByDescending { it.score }
     }
 
-    private fun calculateCosineSimilarity(vector1: Array<Double>, vector2: Array<Double>): Double {
+    private fun calculateDocumentSimilarity(documentA: Document, documentB: Document): Double {
+        val documentVectors: Pair<DoubleArray, DoubleArray> = retrieveDocumentVectors(documentA, documentB)
+        return calculateCosineSimilarity(documentVectors.first, documentVectors.second)
+    }
+
+    private fun retrieveDocumentVectors(documentA: Document, documentB: Document): Pair<DoubleArray, DoubleArray> {
+        return if (useLsi) {
+            latentSemanticIndexer.retrieveDocumentVectors(documentA, documentB)
+        } else {
+            constructDocumentVectors(documentA, documentB)
+        }
+    }
+
+    private fun calculateCosineSimilarity(vectorA: DoubleArray, vectorB: DoubleArray): Double {
         var dotProduct = 0.0
         var normA = 0.0
         var normB = 0.0
 
-        for (i: Int in vector1.indices) {
-            dotProduct += vector1[i] * vector2[i]
-            normA += Math.pow(vector1[i], 2.0)
-            normB += Math.pow(vector2[i], 2.0)
+        for (i: Int in vectorA.indices) {
+            dotProduct += vectorA[i] * vectorB[i]
+            normA += Math.pow(vectorA[i], 2.0)
+            normB += Math.pow(vectorB[i], 2.0)
         }
 
-        val cosineSimilarity: Double = dotProduct / (Math.sqrt(normA) * Math.sqrt(normB))
+        val cosineSimilarity: Double = (dotProduct / (Math.sqrt(normA) * Math.sqrt(normB)))
 
         if (cosineSimilarity == Double.NaN) return 0.0
         return cosineSimilarity
+    }
+
+    private fun constructDocumentVectors(documentA: Document, documentB: Document): Pair<DoubleArray, DoubleArray> {
+        val allWords: List<String> = (documentA.terms + documentB.terms).map { it.word }.distinct().sorted()
+
+        val documentAWordVector: ArrayList<Double> = arrayListOf()
+        val documentBWordVector: ArrayList<Double> = arrayListOf()
+
+        for (word: String in allWords) {
+            documentAWordVector.add(documentA.terms.find { it.word == word }?.tfidf ?: 0.0)
+            documentBWordVector.add(documentB.terms.find { it.word == word }?.tfidf ?: 0.0)
+        }
+
+        return Pair(documentAWordVector.toDoubleArray(), documentBWordVector.toDoubleArray())
     }
 
     private fun documentSimilarityShouldBeCalculated(document1: Document, document2: Document): Boolean {
